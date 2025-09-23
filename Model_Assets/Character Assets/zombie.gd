aa629aacd81f9896f4edba87a7b00f48186aaa58
Sponk <sjarney42@tntech.edu>
2025-09-22 23:50:19 -0500

    all the shit

--- Model_Assets/Character Assets/zombie.gd
+++ Model_Assets/Character Assets/zombie.gd
@@ -1,11 +1,28 @@
-extends Node3D
+extends CharacterBody3D
+
+@onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
+@onready var anim_tree: AnimationTree = $AnimationTree
 
 
-# Called when the node enters the scene tree for the first time.
+var player: Node3D
+const SPEED: float = 4.0
+@export var player_path = "../player"
+const RANGE = 20
+
 func _ready() -> void:
-	pass # Replace with function body.
+	player = get_node(player_path)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	nav_agent.set_target_position(player.global_transform.origin)
+	var next_nav_point = nav_agent.get_next_path_position()
+	var direction = (next_nav_point - global_transform.origin).normalized()
+	velocity = direction * SPEED
+	
+	
+	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z),Vector3.UP)
+	
+	anim_tree.set("parameters/conditions/zombie_run",_player_in_range())
+	move_and_slide()
+	
+func _player_in_range():
+	return global_position.distance_to(player.global_position) < RANGE
